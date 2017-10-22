f683f3cdfb86e5e8ea9636d34317ace03b48095f
Pedro Andrade <pedro.henrique1@gmail.com>
2017-10-22 19:03:11 -0300

    ejs setted

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 const port = 3000
 
 const indexRoutes = require('./routes/index')
 const clientsRoutes = require('./routes/clients')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.use((req, res, next) => {
     console.log('I was here - Middleware')
